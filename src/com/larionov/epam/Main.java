5d58e4d0208f20b37cefba06eb1ac2827190596f
Larionov <Dmitrii_Larionov@epam.com>
2019-12-12 15:38:07 +0400

    Add in class Min Good Morning

--- src/com/larionov/epam/Main.java
+++ src/com/larionov/epam/Main.java
@@ -2,6 +2,6 @@ package com.larionov.epam;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+        System.out.printf("Good morning add-feature1");
     }
 }
